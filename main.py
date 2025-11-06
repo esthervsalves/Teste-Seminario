473825597aa503015b800ba9132f300df6172543
Esther Alves <esthervsalves@gmail.com>
2025-11-06 15:14:55 -0300

    adição

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ def somar(a, b):
 
     return a + b
 
-def subtrair(a, b):
-    return a - b
-
 def executar_operacoes():
     print("--- Operações Base ---")
     print(f"Soma: 10 + 5 = {somar(10, 5)}")
-    print(f"Subtração: 10 - 5 = {subtrair(10, 5)}")
 
 if __name__ == "__main__":
     executar_operacoes()
